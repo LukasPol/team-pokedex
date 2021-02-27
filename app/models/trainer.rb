587c3e784626486135b9358f73da6587647923be
Lukas Pol <lukaspaespol@gmail.com>
2021-02-26 23:49:14 -0300

    create model and tests for trainer

---
+++ app/models/trainer.rb
@@ -0,0 +1,10 @@
+class Trainer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name,presence: true
+
+  enum gender: [:male, :female, :other]
+end
