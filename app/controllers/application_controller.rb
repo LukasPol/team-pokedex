0331f206e9771ecf6e1ed8ce62f7c50727796bdc
Lukas Pol <lukaspaespol@gmail.com>
2021-02-28 23:06:06 -0300

    include pundit to authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_trainer!
 
+  # Pundit
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -20,6 +24,16 @@ class ApplicationController < ActionController::Base
     end
 
     def devise_registration_edit_view?
-      request.fullpath.match?('/managers/edit') || (request.fullpath.match('/managers') && params[:action] == 'update')
+      request.fullpath.match?('/trainers/edit') || (request.fullpath.match('/trainers') && params[:action] == 'update')
+    end
+
+    # PUNDIT: Change current_user to current_trainer 
+    def pundit_user
+      current_trainer
+    end
+
+    def user_not_authorized
+      flash[:alert] = I18n.t(:you_are_not_authorized_to_perform_this_action)
+      redirect_to(request.referer || home_path)
     end
 end
